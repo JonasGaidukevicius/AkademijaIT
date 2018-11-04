719e9fd107f034caee3964c4a46f50a63aba0859
Jonas <jgaidukevicius@gmail.com>
2018-11-04 21:31:34 +0200

    2018.11.04 vakare is namu

--- pamoka_20181029/src/lt/vtvpmc/ems/akademijait/rekursija/ScannerFileReader.java
+++ pamoka_20181029/src/lt/vtvpmc/ems/akademijait/rekursija/ScannerFileReader.java
@@ -12,22 +12,51 @@ public class ScannerFileReader {
 
     public static void readFileOne()throws Exception{
         String separator = File.separator;
-        String path = separator + "home" + separator + "jonas" + separator + "Desktop" + separator + "java_desktop.txt";
+        String path = separator + "home" + separator + "jonasnamai" + separator + "Desktop" + separator + "java_desktop.txt";
         File file = new File(path);
         Scanner reader = new Scanner(file);
 
+        BufferedWriter out = new BufferedWriter(new FileWriter("java_is_desktop.txt"));
+        out.write("Write the string to text file");
+        out.newLine();
+
+
+        //taip neveikia - failas tuscias
+        while(reader.hasNext()){
+            //System.out.println(reader.nextLine());
+            String line = reader.nextLine();
+            String naujaEilute = "/n";
+            System.out.println(line);
+
+            //copiedFile.write(line);
+            out.write(line);
+            out.newLine();
+        }
+
+
+
+
+
+
+        /* Cia budas su FileOutputStream*/
         FileOutputStream fos = new FileOutputStream("java_is_desktop.txt");
         DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(fos));
+
+
 
         while(reader.hasNext()){
             //System.out.println(reader.nextLine());
             String line = reader.nextLine();
             String naujaEilute = "/n";
             System.out.println(line);
+
+            fos.write(line.getBytes(), 0, line.length());
+
             //copiedFile.write(line);
-            outStream.writeUTF(line);
-            outStream.writeUTF(naujaEilute);
+            //outStream.writeUTF(line);
+            //outStream.writeUTF(naujaEilute);
         }
+        fos.close();
         outStream.close();
     }
 }
---
+++ pamoka_20181029/src/lt/vtvpmc/ems/akademijait/rekursija/ScannerFileReader_V2.java
@@ -0,0 +1,46 @@
+package lt.vtvpmc.ems.akademijait.rekursija;
+// SITA REIKIA PABAIGTI!!!!!!
+import java.io.*;
+import java.util.Scanner;
+
+public class ScannerFileReader_V2 {
+
+    public static void main(String[] args) throws Exception{
+        readFileOne();
+
+    }
+
+    public static void readFileOne()throws Exception{
+        String separator = File.separator;
+        String path = separator + "home" + separator + "jonasnamai" + separator + "Desktop" + separator + "java_desktop.txt";
+        File file = new File(path);
+        Scanner reader = new Scanner(file);
+
+
+        OutputStream os = null;
+        try {
+
+            os = new FileOutputStream(new File("java_is_desktop.txt"));
+            while(reader.hasNext()){
+                String line = reader.nextLine();
+                System.out.println(line);
+                System.out.println(line.getBytes());
+                os.write(line.getBytes(), 1, line.length());
+                os.write(Integer.parseInt("Tastes"));
+                os.write(1);
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
